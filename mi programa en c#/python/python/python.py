da2f3251ab9dec03b7931e068e090c053e844075
Melamaya <mel.amaya@ucb.edu.bo>
2024-08-16 10:36:40 -0400

    Create python.py
    programacion en python

---
+++ mi programa en c#/python/python/python.py
@@ -0,0 +1,14 @@
+def Holamundo():
+    print("hola mundo")
+#obtener edad 
+def obteneredad():
+    obteneredad=input("ingrese su edad")
+    print ("su edda es ", obteneredad)
+#calcular edad
+def calcularedad():
+    calcularedad= int(input("ingrese el año que nacio "))
+    edad=2024-calcularedad
+    print ("su edad es",2024-edad)
+Holamundo()
+obteneredad()
+calcularedad()
