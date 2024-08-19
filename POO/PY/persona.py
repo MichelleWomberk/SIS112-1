e9d2a417abfece793cdd9acdb7dd71d22af20f03
Melamaya <mel.amaya@ucb.edu.bo>
2024-08-19 12:00:20 -0400

    POO Python

---
+++ POO/PY/persona.py
@@ -0,0 +1,21 @@
+class Persona:
+    def __init__(self, nombre, edad, carrera):
+        self.nombre  = nombre
+        self.edad = edad
+        self.carrera = carrera 
+
+    def saludar (self):
+        print(f"hola,mi nombre es {self.nombre} y tengo {self.edad} años. ")
+    
+    def cumpleaños(self):
+        self.edad += 1
+        print (f"¡feliz cumpleaños! ahora tengo {self.edad} años. ")
+               
+    def estudiar (self):
+        print(f"estoy estudiando{self.carrera}.")
+
+# crear una instacia de la clase persona 
+persona1 = Persona("Juan", 20, "medicina")
+# llamar a los metodos 
+persona1.saludar()
+persona1.estudiar()
