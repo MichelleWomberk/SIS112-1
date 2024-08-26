6034347a037183bbab4ee7796a8c34d03d5b9f15
Melamaya <mel.amaya@ucb.edu.bo>
2024-08-26 12:27:03 -0400

    modificar

--- POO/JS/persona.js
+++ POO/JS/persona.js
@@ -1,30 +1,69 @@
-class Persona{
-    constructor(nombre,edad,carrera){
-        this.nombre = nombre;
-        this.edad = edad ;
-        this.carrera =carrera ;
-
+class Persona {
+    constructor(nombre, edad, carrera,universidad) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.carrera = carrera;
+      this.universidad = universidad;
     }
-
-    saludar (){
-        console.log('hola ,mi nombre es' +this.nombre+'y tengo '+this.nombre+ 'años ');
+  
+    saludar() {
+      return 'Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
     }
-
-    cumpleaños(){
-        this.edad = this.edad + 1;
-        console.log('!feliz cumpleaños ! ahora tengo '+this.edad+' años ');
+  
+    cumpleanios() {
+      return '¡Felicidades! tienes ' + this.edad + ' años.';
     }
-
-    estudiar(){
-        console.log('estoy estudiando'+this.carrera+);
+  
+    estudiar() {
+      return 'Estoy estudiando ' + this.carrera ;
     }
-}
-function SaludoPersona1(){
-     //crear una instancia de la clase persona
-const persona1 = new Persona("Ana",22,"ingeniera informatica");
-// llamar a los metodos
-persona1.saludar();  
-}
+    Universidad() {
+      return 'estudia en ' + this.universidad;
+    }
+    Modificarsaludar() {
+      this.nombre =prompt("ingrese su nombre");
+      return 'Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
+    }
+    Modificarcumpleanios() {
+      this.nombre =prompt("ingrese su edad","19");
+       return '¡Felicidades! tienes ' + this.edad + ' años.'
+    }
+    
+  
+  
+    
+  }
+  
+  // Crear una instancia de la clase Persona
+  const persona = new Persona('Mel', 19, 'Ingeniería industrial','en la universidad Catolica ');
+  
+  // Inicializar los elementos HTML
+  const saludar = document.getElementById('saludar');
+  const edad = document.getElementById('edad');
+  const carrera = document.getElementById('carrera');
+  const universidad= document.getElementById('universidad');
+
+  
+
+  
+  // Función para saludar
+  function botonSaludar() {
+    saludar.textContent = persona.saludar();
+  }
+  
+  // Función para cumpleaños
+  function botonCumpleanios() {
+    edad.textContent = persona.cumpleanios();
+  }
+  
+  // Función para estudiar
+  function botonEstudiar() {
+    carrera.textContent = persona.estudiar();
+  }
+   // Función para estudiar
+   function botonUniversidad() {
+    universidad.textContent = persona.Universidad();
+  }
 
 
 
