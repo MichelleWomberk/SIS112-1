0b990de4739b978ffe471f50f4ed645748206555
Melamaya <mel.amaya@ucb.edu.bo>
2024-08-28 16:22:01 -0400

    tarea 1

--- POO/JS/persona.js
+++ POO/JS/persona.js
@@ -1,9 +1,13 @@
 class Persona {
     constructor(nombre, edad, carrera,universidad) {
-      this.nombre = nombre;
+      this.nombre=nombre;
       this.edad = edad;
       this.carrera = carrera;
       this.universidad = universidad;
+      this.Modificar_nombre=nombre;
+      this.Modificar_edad = edad;
+      this.Modificar_carrera = carrera;
+      this.Modificar_universidad = universidad;
     }
   
     saludar() {
@@ -20,16 +24,34 @@ class Persona {
     Universidad() {
       return 'estudia en ' + this.universidad;
     }
-    Modificarsaludar() {
+    Modificar_Saludo() {
       this.nombre =prompt("ingrese su nombre");
       return 'Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
     }
-    Modificarcumpleanios() {
-      this.nombre =prompt("ingrese su edad","19");
-       return '¡Felicidades! tienes ' + this.edad + ' años.'
+    Modificar_cumpleanios() {
+      this.edad=prompt("ingrese su edad","19");
+      return '¡Felicidades! tienes ' + this.edad + ' años.'
     }
-    
-  
+    Modificar_estudiar() {
+      this.carrera =prompt("ingrese la carrera que esta estudiando ","Ingenieria industrial");
+      return 'Estoy estudiando ' + this.carrera ;
+       }
+    Modificar_Universidad() {
+      this.universidad =prompt("ingrese la universidad donde estudia o estudio","universidad Catolica Boliviana");
+      return 'Estudia en  ' + this.universidad ;
+    }
+    eliminar_saludar() {
+      return  ;
+    }
+    eliminar_cumpleanios() {
+      return  ;
+    }
+    eliminar_estudiar() {
+      return  ;
+    }
+    eliminar_universidad() {
+      return  ;
+    }
   
     
   }
@@ -65,6 +87,42 @@ class Persona {
     universidad.textContent = persona.Universidad();
   }
 
+//Modificar
+
+ // Función para saludar
+  function botonModificar_Saludo() {
+   saludar.textContent = persona.Modificar_Saludo();
+}
+
+// Función para cumpleaños
+  function botonModificar_Edad() {
+   edad.textContent = persona.Modificar_cumpleanios();
+}
+
+// Función para estudiar
+  function botonModificar_Carrera() {
+   carrera.textContent = persona.Modificar_estudiar();
+}
+ // Función para estudiar
+  function botonModificar_Universidad() {
+   universidad.textContent = persona.Modificar_Universidad();
+}
+// Función para saludar
+function botoneliminar_Saludo() {
+  saludar.textContent = persona.eliminar_saludar();
+  }
+// Función para saludar
+function botoneliminar_Edad() {
+  edad.textContent = persona.eliminar_cumpleanios();
+  }
+  // Función para saludar
+function botoneliminar_Carrera() {
+  carrera.textContent = persona.eliminar_estudiar();
+  }
+  // Función para saludar
+function botoneliminar_Universidad() {
+  universidad.textContent = persona.eliminar_universidad();
+  }
 
 
 
